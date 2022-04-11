ae41f121f695900159482fa9987461060b7637a4
Buby Speed <Buby123@gmx.de>
2022-04-11 19:03:55 +0200

    implemented GameMaster and added abstract class Player

--- GameMaster.java
+++ GameMaster.java
@@ -24,17 +24,43 @@ public class GameMaster
         list_player.remove(player);
     }
     
-    public ArrayList<Player> game(){
+    public Player[] play_game(){
+        int current_throw = 1 + (int)Math.random()*6;
+        int current_values[] = new int[list_player.size()];
+        int num_rated[] = new int[list_player.size()];
+        
         for(int i=0; i<13; i++){
-            int aktWurf = 1 + (int)Math.random()*6;
-            
-            for(int x=0; x<list_player.length; x++){
-                
+            for(int j=0; j<list_player.size(); j++){
+                list_player.get(j).update_gamedata(current_values[j], num_rated[j]);
+                if(i-num_rated[j] >= 5) {
+                    current_values[j] += current_throw;
+                    num_rated[j]++;
+                }
+                else if(list_player.get(j).rate_throw(current_throw) && num_rated[j] < 8) {
+                    current_values[j] += current_throw;
+                    num_rated[j]++;
+                }
             }
+        }
+        for(int i=0; i<list_player.size(); i++) {
+            current_values[i] = Math.abs(current_values[i]-22);
+        }
+        Player ranked_players[] = new Player[list_player.size()];
+        for(int i=0; i<list_player.size(); i++) {
+            int best_value = Integer.MAX_VALUE;
+            int best_index = -1;
+            for(int j=0; j<list_player.size(); j++) {
+                if(current_values[j] < best_value) {
+                    best_value = current_values[j];
+                    best_index = j;
+                }
+            }
+            ranked_players[i] = list_player.get(best_index);
+            current_values[i] = Integer.MAX_VALUE;
         }
         
         
-        return list_player;
+        return ranked_players;
     }
     // Anzahl Test festlegen 
     
--- Player.java
+++ Player.java
@@ -1,7 +1,12 @@
 
 public abstract class Player
 {
-    public Player(){
-        
+    int current_value;
+    int num_rated;
+    abstract boolean rate_throw(int rolled_dice);
+    
+    public void update_gamedata(int current_value, int num_rated){
+        this.current_value = current_value;
+        this.num_rated = num_rated;
     }
 }
