37940fe3e31f9d27fc4e4bb419685a01f14e1f06
Buby Speed <Buby123@gmx.de>
2022-04-11 20:19:34 +0200

    added Bot_Tim, fixed bug where only 1 was thrown

--- Player.java
+++ Player.java
@@ -1,14 +1,20 @@
 
 public abstract class Player
 {
-    int current_value;
-    int num_rated;
-    int num_throws;
+    protected String name = "";
+    protected int current_value;
+    protected int num_rated;
+    protected int num_throws;
+    
     abstract boolean rate_throw(int rolled_dice);
     
     public void update_gamedata(int current_value, int num_rated, int num_throws){
         this.current_value = current_value;
         this.num_rated = num_rated;
         this.num_throws = num_throws;
+    }
+    
+    public String get_name() {
+        return name;
     }
 }
